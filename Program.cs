eff51c98a5b28289b39dbe977d1ac545a31e3c21
Xi-v <72281205+Xi-v@users.noreply.github.com>
2021-04-28 23:26:27 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace Examples.System.Net {
+  public class HttpWebGetPost {
+    public static void Main() {
+      int requestSize = 30;
+      var targethost = "https://example.com";
+      string postString = @ "0884892649966840111330768081643308848926499668401";
+      for (int i = 0; i < requestSize; i++) {
+        WebRequest webrequest = WebRequest.Create(targethost);
+        webrequest.Credentials = CredentialCache.DefaultCredentials;
+        WebResponse response = webrequest.GetResponse();
+        Console.WriteLine(string.Format("{0}", i));
+        Console.WriteLine(((HttpWebResponse) response).StatusDescription);
+        response.Close();
+        using(WebClient client = new WebClient()) {
+          client.UploadValues(targethost, new NameValueCollection() {
+            {
+              "username",
+              postString
+            }
+          });
+
+        }
+
+      }
+    }
+  }
+}
